07e9ccd4c0f49ce9ccfec964c4b88f2bac305630
sjordan98 <sjordan@middlebury.edu>
2017-07-12 11:07:46 -0400

    Create somejava.js

---
+++ somejava.js
@@ -0,0 +1,6 @@
+function myFunction(){
+  document.getElementByID("questionparagraph").innerHTML = "question1";
+  document.createElement(button).innerHTML="option1";
+  document.createElement(button).innerHTML="option2";
+}
+

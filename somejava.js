8fb84488294950addc8ac4dfcb361cddfa573465
sjordan98 <sjordan@middlebury.edu>
2017-07-12 14:43:09 -0400

    Update somejava.js

--- somejava.js
+++ somejava.js
@@ -1,10 +1,27 @@
 
 
-document.createElement("BUTTON");
+var total=0;
 
-function myFunction(){
-  document.getElementByID("questionparagraph").innerHTML = "question1";
-  document.write("HI")
+function myFunction(button.id){
+  if (button.id=="A"){
+    total+=5;
+  }
+  else if (button.id=="B"){
+    total+=3;
+  }
+  else if (button.id=="C"){
+    total+=1;
+  }
+}
+
+if (total>50){
+ document.getElementById("answer")==X; 
+}
+else if (total>40){
+ document.getElementById("answer")==X; 
+}
+else if (total>30){
+ document.getElementById("answer")==X; 
 }
 
 

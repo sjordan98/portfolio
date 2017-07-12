7c3d387e53e1b7023b68678cf6d02e9b814152ab
sjordan98 <sjordan@middlebury.edu>
2017-07-12 13:23:13 -0400

    Update somejava.js

--- somejava.js
+++ somejava.js
@@ -1,12 +1,10 @@
 
 
-document.getElementById("demo").innerHTML = 5 + 6;
-document.createElement("BUTTON").innerHTML="steph";
+document.createElement("BUTTON");
 
 function myFunction(){
   document.getElementByID("questionparagraph").innerHTML = "question1";
-  document.getElementByID("button1").innerHTML="option1";
-  document.getElementByID("button2").innerHTML="option2";
+  document.write("HI")
 }
 
 
